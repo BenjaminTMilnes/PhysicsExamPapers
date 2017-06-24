96e42c4eaa3b5fc74c472cab42a12a7809d93868
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2017-06-24 18:23:57 +0100

    Added XMLImporter to handle the loading of XML documents in a separate static contents folder

---
+++ PhysicsExamPapers.Content/XMLImporter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace PhysicsExamPapers.Content
+{
+    public sealed class XMLImporter
+    {
+        private string _basePath;
+
+        public XMLImporter(string basePath)
+        {
+            _basePath = basePath;
+        }
+
+        public XMLResource Import(string resourceName)
+        {
+            var path = $"{_basePath}{resourceName}.xml";
+            var document = new XmlDocument();
+
+            document.Load(path);
+
+            return new XMLResource(document);
+        }
+    }
+}
