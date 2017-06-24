e2451cbd8abb40ea117b16a5195a6597831b2563
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2017-06-24 16:33:35 +0100

    Started creating a framework to allow combined use ot static content files with dynamically generated sections

---
+++ PhysicsExamPapers.Content/Physics/GeneralRelativity/EvaluateTheKroneckerDelta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysicsExamPapers.Content.Physics.GeneralRelativity
+{
+    public class EvaluateTheKroneckerDelta :  QuestionGenerator
+    {
+        public EvaluateTheKroneckerDelta()
+        {
+            StaticContentName = "physics__general_relativity__evaluate_the_kronecker_delta";
+        }
+        
+        public override IQuestion Generate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PhysicsExamPapers.Content/QuestionGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysicsExamPapers.Content
+{
+    public abstract class QuestionGenerator : IQuestionGenerator
+    {
+        protected string StaticContentName { get; set; }
+
+        public abstract IQuestion Generate();
+    }
+}
