aff153456662702f6934b9dfbc8e07ca16a47471
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2017-06-24 18:14:17 +0100

    Added XMLResource to handle getting data out of the XML static resource files

---
+++ PhysicsExamPapers.Content/XMLResource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace PhysicsExamPapers.Content
+{
+    public class XMLResource
+    {
+        protected XmlDocument _document;
+
+        public XMLResource(XmlDocument document)
+        {
+            _document = document;
+        }
+
+        public string GetQuestionContent()
+        {
+            return _document.SelectSingleNode("/question/content").InnerXml;
+        }
+    }
+}
