3502c272c5da2c59b080a35e44c85affc12bd7f5
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2017-06-24 18:30:55 +0100

    Added Question to implement IQuestion

--- PhysicsExamPapers.Content/Physics/GeneralRelativity/EvaluateTheKroneckerDelta.cs
+++ PhysicsExamPapers.Content/Physics/GeneralRelativity/EvaluateTheKroneckerDelta.cs
@@ -6,15 +6,27 @@ using System.Threading.Tasks;
 
 namespace PhysicsExamPapers.Content.Physics.GeneralRelativity
 {
-    public class EvaluateTheKroneckerDelta :  QuestionGenerator
+    public class EvaluateTheKroneckerDelta : QuestionGenerator
     {
-        public EvaluateTheKroneckerDelta()
+        protected XMLImporter _xmlImporter;
+
+        public EvaluateTheKroneckerDelta(XMLImporter xmlImporter)
         {
+
+            _xmlImporter = xmlImporter;
+
             StaticContentName = "physics__general_relativity__evaluate_the_kronecker_delta";
         }
-        
+
         public override IQuestion Generate()
         {
+
+            var xmlResource = _xmlImporter.Import(StaticContentName);
+
+
+
+
+
             throw new NotImplementedException();
         }
     }
---
+++ PhysicsExamPapers.Content/Question.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysicsExamPapers.Content
+{
+    public class Question : IQuestion
+    {
+        public string Content { get; set; }
+    }
+}
