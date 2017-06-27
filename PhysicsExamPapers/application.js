aea1555a79210574102075cbead5310763e66b77
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-06-27 21:02:23 +0100

    Started making the main application to give the questions to the user

---
+++ PhysicsExamPapers/application.js
@@ -0,0 +1,7 @@
+﻿var application = angular.module("PhysicsExamPapers", []);
+
+application.controller("ExamController", ["$scope", "$http", function ($scope, $http) {
+
+    $scope.examTemplate = {};
+
+}]);
