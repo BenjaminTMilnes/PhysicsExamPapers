dc41d6bbcfe9413a0f8a99f037fe43ddb2a8ed4e
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-07-06 20:34:40 +0100

    Begun adding basic exam flow control to the ExamController

--- PhysicsExamPapers/application.js
+++ PhysicsExamPapers/application.js
@@ -1,9 +1,22 @@
 ﻿var application = angular.module("PhysicsExamPapers", []);
 
+var ExamPositions = { Introduction: 0, Questions: 1, Conclusion: 2 }
+
 application.controller("ExamController", ["$scope", "$http", function ($scope, $http) {
 
     $scope.examTemplate = {};
+    $scope.examPosition = ExamPositions.Introduction;
+    $scope.partNumber = 0;
+    $scope.questionNumber = 0;
 
+    $scope.getExamTemplate = function (reference) {
+
+        reference = "static_content/Physics_GeneralRelativity.json";
+
+        $http.get(reference).then(function (response) {
+            return response.data;
+        })
+    }
 
     $scope.getQuestion = function (reference) {
         $http.get("api/questions/1").then(function (response) {
