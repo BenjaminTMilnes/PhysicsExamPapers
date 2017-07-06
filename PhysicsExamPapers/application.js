29e614f7975217507f01c38692ddc42a5563fbd0
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-07-06 20:26:23 +0100

    Added the first exam template for General Relativity

--- PhysicsExamPapers/application.js
+++ PhysicsExamPapers/application.js
@@ -4,4 +4,11 @@ application.controller("ExamController", ["$scope", "$http", function ($scope, $
 
     $scope.examTemplate = {};
 
+
+    $scope.getQuestion = function (reference) {
+        $http.get("api/questions/1").then(function (response) {
+            return response.data;
+        });
+    };
+
 }]);
