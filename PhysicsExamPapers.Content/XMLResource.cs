488299852c8ba2313c58cdef31504f875d522844
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-07-18 22:04:29 +0100

    Modified XMLResource to allow getting data about answers from the template

--- PhysicsExamPapers.Content/XMLResource.cs
+++ PhysicsExamPapers.Content/XMLResource.cs
@@ -21,6 +21,36 @@ namespace PhysicsExamPapers.Content
             return _document.SelectSingleNode("/question/content").InnerXml;
         }
 
+        public int GetNumberOfCorrectAnswers()
+        {
+            return _document.SelectNodes("/question/answers/correct-answers/answer").Count;
+        }
+
+        public string GetCorrectAnswerType(int index)
+        {
+            return _document.SelectSingleNode($"/question/answers/correct-answers/answer[{index}]").Attributes["type"].Value;
+        }
+
+        public string GetCorrectAnswerContent(int index)
+        {
+            return _document.SelectSingleNode($"/question/answers/correct-answers/answer[{index}]").InnerXml;
+        }
+
+        public int GetNumberOfIncorrectAnswers()
+        {
+            return _document.SelectNodes("/question/answers/incorrect-answers/answer").Count;
+        }
+
+        public string GetIncorrectAnswerType(int index)
+        {
+            return _document.SelectSingleNode($"/question/answers/incorrect-answers/answer[{index}]").Attributes["type"].Value;
+        }
+
+        public string GetIncorrectAnswerContent(int index)
+        {
+            return _document.SelectSingleNode($"/question/answers/incorrect-answers/answer[{index}]").InnerXml;
+        }
+
         public int GetNumberOfHints()
         {
             return _document.SelectNodes("/question/hints/hint").Count;
