1353ff8563ee5ba32908a895009fc49ea1bd6da0
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2017-07-22 09:23:50 +0100

    Added NonRandomQuestionGenerator for questions that can only have one form

---
+++ PhysicsExamPapers.Content/NonRandomQuestionGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhysicsExamPapers.Content.Layout;
+
+namespace PhysicsExamPapers.Content
+{
+    public class NonRandomQuestionGenerator : QuestionGenerator
+    {
+        public string XMLTemplateReference { get; protected set; }
+
+        public NonRandomQuestionGenerator(string xmlTemplateReference, XMLImporter xmlImporter, TextResolver textResolver, LayoutConverter layoutConverter) : base(xmlImporter, textResolver, layoutConverter)
+        {
+            XMLTemplateReference = xmlTemplateReference;
+        }
+
+        public override IQuestion Generate(Random random)
+        {
+            var model = new Model();
+
+            return Generate(model);
+        }
+
+        public override IQuestion Generate(Model model)
+        {
+            var xmlTemplate = GetXMLTemplate(XMLTemplateReference);
+
+            return Generate(xmlTemplate, model);
+        }
+    }
+}
