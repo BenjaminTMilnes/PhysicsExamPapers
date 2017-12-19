902490579f74fcf7bae1c353c2028e3ad0550552
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-12-19 19:30:08 +0000

    Added a question to Waves

--- PhysicsExamPapers.Content/Physics/GeneralRelativity/QuestionSet.cs
+++ PhysicsExamPapers.Content/Physics/GeneralRelativity/QuestionSet.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using PhysicsExamPapers.Content.Layout;
 using PhysicsExamPapers.Content.Physics.NewtonianGravity;
+using PhysicsExamPapers.Content.Physics.Waves;
 
 namespace PhysicsExamPapers.Content.Physics.GeneralRelativity
 {
@@ -15,6 +16,7 @@ namespace PhysicsExamPapers.Content.Physics.GeneralRelativity
             Generators.Add(GetGeneratorXMLTemplateReference(typeof(EvaluateTheKroneckerDelta)), new EvaluateTheKroneckerDelta(_xmlImporter, _textResolver, _layoutConverter));
             Generators.Add(GetGeneratorXMLTemplateReference(typeof(SimplifyKroneckerDeltas)), new SimplifyKroneckerDeltas(_xmlImporter, _textResolver, _layoutConverter));
             Generators.Add(GetGeneratorXMLTemplateReference(typeof(SimplifyKroneckerDeltas2)), new SimplifyKroneckerDeltas2(_xmlImporter, _textResolver, _layoutConverter));
+            Generators.Add(GetGeneratorXMLTemplateReference(typeof(CalculateTheBeatFrequency1)), new CalculateTheBeatFrequency1(_xmlImporter, _textResolver, _layoutConverter));
 
             AddNonRandomQuestionGenerator("Physics_NewtonianGravity_DefineThePoissonEquation");
             AddNonRandomQuestionGenerator("Mathematics_Hyperbolae_DefineAHyperbola1");
---
+++ PhysicsExamPapers.Content/Physics/Waves/CalculateTheBeatFrequency1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhysicsExamPapers.Content.Layout;
+
+namespace PhysicsExamPapers.Content.Physics.Waves
+{
+    [XMLTemplateReference("Physics_Waves_CalculateTheBeatFrequency1")]
+    public class CalculateTheBeatFrequency1 : QuestionGenerator
+    {
+        public CalculateTheBeatFrequency1(XMLImporter xmlImporter, TextResolver textResolver, LayoutConverter layoutConverter) : base(xmlImporter, textResolver, layoutConverter) { }
+
+        public override IQuestion Generate(Random random)
+        {
+            var f1 = GenerateRandomNumberBetweenLimits(random, 50, 2000);
+            var df = GenerateRandomNumberBetweenLimits(random, -12, 12);
+            var f2 = f1 + df;
+            var beatFrequency = Math.Abs(2 * df);
+
+            var model = new Model();
+            model.Add("f1", f1);
+            model.Add("f2", f2);
+            model.Add("df", df);
+            model.Add("bf", beatFrequency);
+
+            return Generate(model);
+        }
+
+        public override IQuestion Generate(Model model)
+        {
+            var type = typeof(CalculateTheBeatFrequency1);
+            var xmlTemplateReference = GetXMLTemplateReference(type);
+            var xmlTemplate = GetXMLTemplate(xmlTemplateReference);
+
+            return Generate(xmlTemplate, model);
+        }
+
+        protected override IList<IAnswer> CalculateCorrectAnswers(XMLResource xmlTemplate, Model model)
+        {
+            var correctAnswers = new List<IAnswer>();
+
+            var correctAnswer = new Answer();
+            correctAnswer.Type = AnswerType.Number;
+            correctAnswer.Content = (int)model["bf"];
+
+            correctAnswers.Add(correctAnswer);
+
+            return correctAnswers;
+        }
+    }
+}
